cfaeb377f7536da1bfa277b25d075d5fb11c38b2
Abhishek Ganeshe <abhishekganeshe33@gmail.com>
2025-05-20 13:09:16 +0530

    updated PushNotification API Logic in common controller

--- configs/fcm.config.js
+++ configs/fcm.config.js
@@ -2,14 +2,6 @@ const admin= require('firebase-admin')
 const serviceAccount = require('./dfic-notification-firebase-adminsdk-fbsvc-1be811060a.json')
 
 
-// exports.handleConnectFcmForPushNotifications= async()=>{
-//     try{
-        
-//     }catch(err){
-
-//     }
-// }
-
 admin.initializeApp({
     credential: admin.credential.cert(serviceAccount),
 })
